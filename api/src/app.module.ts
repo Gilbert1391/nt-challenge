4622e4a257b5849cfac2ade5c34e57fddb896d78
Gilbert Rosario <pro.devgilbert@gmail.com>
2020-05-25 15:16:13 -0400

    pre prod confg

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ContactsModule } from './contacts/contacts.module';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/newtech', {
+    MongooseModule.forRoot(dbConfig, {
       useNewUrlParser: true,
     }),
     ContactsModule,
--- web/src/app/services/contacts/contact.service.ts
+++ web/src/app/services/contacts/contact.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class ContactService {
+  apiUrl = environment.apiUrl;
+  apiRoute = "/contacts/";
+
   constructor(private http: HttpClient) {}
 
   getContacts(): Observable<IApiResponse> {
-    return this.http.get<IApiResponse>("http://localhost:3000/contacts");
+    return this.http.get<IApiResponse>(`${this.apiUrl}${this.apiRoute}`);
   }
 
   getContactById(id: string): Observable<IContact> {
-    return this.http.get<IContact>(`http://localhost:3000/contacts/${id}`);
+    return this.http.get<IContact>(`${this.apiUrl}${this.apiRoute}${id}`);
   }
 
   createContact(contact: IContact): Observable<IContact> {
-    return this.http.post<IContact>("http://localhost:3000/contacts", contact);
+    return this.http.post<IContact>(`${this.apiUrl}${this.apiRoute}`, contact);
   }
 
   updateContact(id: string, contact: IContact): Observable<void> {
-    return this.http.put<void>(`http://localhost:3000/contacts/${id}`, contact);
+    return this.http.put<void>(`${this.apiUrl}${this.apiRoute}${id}`, contact);
   }
 
   deleteContact(id: string): Observable<void> {
-    return this.http.delete<void>(`http://localhost:3000/contacts/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}${this.apiRoute}${id}`);
   }
 }
 
